b6d23b058575bf80ec2f31664558dca47e328a65
mroberts764 <michael.roberts80@bcmail.cuny.edu>
2025-03-17 00:34:15 -0400

    Added constructor and functions from the Point.h header file

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,18 @@
 #include "Point.h"
 
 using namespace std;
+
+Point::Point(double xCoord, double yCoord) {
+    x = xCoord;
+    y = yCoord;
+}
+
+double Point::distanceTo(const Point& other) const
+{
+    return sqrt(pow(other.x - x, 2) + pow(other.y - y, 2));
+}
+
+void Point::print() const
+{
+    cout << "(" << x << ", " << ")" << endl;
+}
